22c932218481e8ca0e1fb752301e6b4a5db2fd79
Elihu <elihusela@mail.tau.ac.il>
2020-04-15 21:49:42 +0300

    fix

--- Datasets.py
+++ Datasets.py
@@ -350,7 +350,6 @@ class NIH_partial_Dataset(Dataset):
         self.img_classes = df['Finding Labels'].values
         self.img_list = []
         self.txt_path = csvpath
-        self.current_package = current_package
         self.transform = transform
         self.to_tensor = transforms.ToTensor()
         self.to_pil = transforms.ToPILImage()
