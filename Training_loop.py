8cc2f07e5c16f7d17cb37942333bb1e736b673af
Elihu <elihusela@mail.tau.ac.il>
2020-04-19 02:16:06 +0300

    device fix

--- Training_loop.py
+++ Training_loop.py
@@ -19,7 +19,8 @@ def set_parameter_requires_grad(model, feature_extracting):
 
 
 
-def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False, device='cpu',TO_FILTER=True, f_block=None):
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False,TO_FILTER=True, f_block=None):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     since = time.time()
 
     val_acc_history = []
